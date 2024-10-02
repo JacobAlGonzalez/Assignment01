0d7a72a76a31cb3549e714cca7d73d9cf8458fc2
Jacob Gonzalez <jacobagonzalez04@gmail.com>
2024-10-01 18:09:50 -0700

    added a new file and folder

---
+++ js/script.js
@@ -0,0 +1,73 @@
+//step 1
+let someMonth
+
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+//step 2
+2.5 
+'hello'
+true
+null
+
+//step 3
+let number = 3 + 4
+
+
+//step 4
+let strFirstName
+let strLastName
+let strAddress
+let strCity
+let strState
+let zipCode
+let intYourAge
+let strReferralSource
+let blnMayWeContactYou
+
+//step 5
+strFirstName = ('Jacob')
+strLastName = ('Gonzalez')
+blnMayWeContactYou = false
+
+//step 6
+let numberString = 20
+alert ('Jacob is ' + numberString + ' years old.')
+
+//step 7
+let variableOne = false
+alert ('the math problem is ' + variableOne)
+let a = 5
+alert (a >= 5 != true) 
+
+//step 8 All I did to fix this line was add a backslash so "I'm" would show
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString)
+
+//step 9
+let x = null
+alert (x)
+let y 
+alert (y)
+
+//step 10
+typeof 'hello'
+typeof 10
+typeof true
+typeof (y)
+typeof null
+
+//step 11
+let z = ('Hello')
+let f = ('Jacob Gonzalez, ')
+let j = ('welcome to the JavaScript class!')
+alert (z + f + j)
+ 
+//step 12
+let name = ('Jacob')
+alert (z + name + j)
+
+//step 13
+let course = ('JavaScript')
+alert (z + name + j + course)
